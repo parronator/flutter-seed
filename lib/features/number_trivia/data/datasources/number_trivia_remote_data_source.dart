d17722d10fdbca7fc7a44e9660a6f700165c71c0
parronator <al.parron@gmail.com>
2020-01-24 22:12:38 +0100

    Added data source implementation

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -1,7 +1,35 @@
+import 'dart:convert';
+
+import 'package:flutter_seed/core/error/exceptions.dart';
 import 'package:flutter_seed/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:meta/meta.dart';
 
 abstract class NumberTriviaRemoteDataSource {
   Future<NumberTriviaModel> getConcreteNumberTrivia(int number);
 
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  final http.Client client;
+  final baseUrl = "http://numbersapi.com";
+
+  NumberTriviaRemoteDataSourceImpl({@required this.client});
+
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) async {
+    return _getTriviaFromUrl('http://numbersapi.com/$number');
+  }
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() async {
+    return _getTriviaFromUrl('http://numbersapi.com/random');
+  }
+
+  Future<NumberTriviaModel> _getTriviaFromUrl(String url) async {
+    final response = await client.get(url, headers: {'Content-Type': 'application/json'});
+    if (response.statusCode != 200) throw ServerException();
+    return NumberTriviaModel.fromJson(json.decode(response.body));
+  }
 }
--- test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -17,12 +17,11 @@ void main() {
     test('should return a valid model when the JSON number is an integer', () async {
       final Map<String, dynamic> jsonMap = json.decode(fixture('trivia.json'));
       final result = NumberTriviaModel.fromJson(jsonMap);
-
       expect(result, tNumberTriviaModel);
     });
 
-    test(
-        'should return a valid model when the JSON number is regarded as a double', () async {
+    test('should return a valid model when the JSON number is regarded as a double',
+        () async {
       final Map<String, dynamic> jsonMap = json.decode(fixture('trivia_double.json'));
       final result = NumberTriviaModel.fromJson(jsonMap);
 

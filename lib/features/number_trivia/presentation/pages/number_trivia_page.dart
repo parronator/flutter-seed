87dac8c572c42394cf8e8390fdfc8f86af9c4230
parronator <al.parron@gmail.com>
2020-01-29 16:37:29 +0100

    Added UI

---
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_seed/features/number_trivia/presentation/bloc/bloc.dart';
+import 'package:flutter_seed/features/number_trivia/presentation/widgets/widgets.dart';
+import 'package:flutter_seed/injection_container.dart' as di;
+
+class NumberTriviaPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Number Trivia'),
+      ),
+      body: SingleChildScrollView(
+        child: buildBody(context),
+      ),
+    );
+  }
+
+  BlocProvider<NumberTriviaBloc> buildBody(BuildContext context) {
+    return BlocProvider(
+      create: (_) => di.sl<NumberTriviaBloc>(),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 10),
+              // Top half
+              BlocBuilder<NumberTriviaBloc, NumberTriviaState>(
+                builder: (context, state) {
+                  if (state is Empty) {
+                    return MessageDisplay(message: 'Start searching!');
+                  } else if (state is Loading) {
+                    return LoadingWidget();
+                  } else if (state is Error) {
+                    return MessageDisplay(message: state.message);
+                  } else if (state is Loaded) {
+                    return TriviaDisplay(numberTrivia: state.trivia);
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              // Bottom half
+              TriviaControls()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
